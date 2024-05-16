00ba6207d91b991fbb9e7188bb833e57e1813b70
MPG2003 <159909720+MPG2003@users.noreply.github.com>
2024-05-16 09:59:07 +0530

    Program.vb

--- Program.vb
+++ Program.vb
@@ -1,72 +1,81 @@
-Imports System
-
 Public Class Student
-    Public Name As String
-    Public Age As Integer
-    Public Grade As Double
-End Class
+        Public Name As String
+        Public Emp_id As Integer
+        Public salary As Char
 
-Public Class Program
-    Private Sub DisplayMenu()
-        Console.WriteLine("1. Add student")
-        Console.WriteLine("2. Display students")
-        Console.WriteLine("3. Delete students")
+        ' Constructor to initialize the Employee object
+        Public Sub New(name As String, stu_id As Integer, address As Char)
+            Me.Name = name
+            Me.stu_id = stu_id
+            Me.address =ad
 
-        Console.Write("Enter your choice: ")
-    End Sub
+        End Sub
 
-    Private Sub AddStudent(students As List(Of Student))
-        Console.Write("Enter student name: ")
-        Dim name As String = Console.ReadLine()
-        Console.Write("Enter student age: ")
-        Dim age As Integer = Int(Console.ReadLine())
-        Console.Write("Enter student grade: ")
-        Dim grade As Double = Double.Parse(Console.ReadLine())
+        ' Function to display student details
+        Public Sub DisplayDetails()
+            Console.WriteLine("Name: " & Name)
+            Console.WriteLine("stu_id: " & stu_id)
+        Console.WriteLine("address:" & ad)
+        Console.WriteLine()
+        End Sub
+    End Class
 
-        students.Add(New Student With {.Name = name, .Age = age, .Grade = grade})
-    End Sub
+    Module Program
+    Sub Main()
+        Dim stu As New List(Of students)()
 
-    Private Sub DisplayStudents(students As List(Of Student))
-        Console.WriteLine("Name     Age     Grade")
-        For Each student As Student In students
-            Console.WriteLine($"{student.Name}       {student.Age}      {student.Grade}")
-        Next
-    End Sub
-    Private Sub DeleteStudents(students As List(Of Student))
-        Console.Write("Enter the name of the student to delete: ")
-        Dim name As String = Console.ReadLine()
-        Dim student As Student = students.FirstOrDefault(Function(s) s.Name = name)
-        If student IsNot Nothing Then
-            students.Remove(student)
-        Else
-            Console.WriteLine("Student not found")
-        End If
-    End Sub
+        Dim choice As Integer
 
-    Public Sub Run()
-        Dim students As New List(Of Student)
-        While True
-            DisplayMenu()
-            Dim choice As Integer = Int(Console.ReadLine())
+        Do
+            Console.WriteLine("Menu:")
+            Console.WriteLine("1. Add students")
+            Console.WriteLine("2. Display All students")
+            Console.WriteLine("3. Delete")
+            Console.WriteLine("4. Exit")
+            Console.Write("Enter your choice: ")
+            choice = Console.ReadLine()
+
             Select Case choice
                 Case 1
-                    AddStudent(students)
-                Case 2
-                    DisplayStudents(students)
-                Case 3
-                    DeleteStudents(students)
+                    Console.Write("Enter student name: ")
+                    Dim name As String = Console.ReadLine()
+                    Console.Write("Enter student stu_id: ")
+                    Dim Emp_id As Integer = Convert.ToInt32(Console.ReadLine())
+                    Console.Write("Enter  address: ")
+                    Dim salary As Char = Console.ReadLine()(0)
 
+                    ' Create a new Employee object and add it to the list
+                    Dim employee As New Student(name, stu_id, ad)
+                    Emp.Add(employee)
+                    Console.WriteLine("student added successfully.")
+                    Console.WriteLine()
+                Case 2
+                    If Emp.Count > 0 Then
+                        Console.WriteLine("Details of all student:")
+                        For Each student As Student In Stu
+                            student.DisplayDetails()
+                        Next
+                    Else
+                        Console.WriteLine("No student  added yet.")
+                    End If
+                    Console.WriteLine()
+                Case 3
+                    If stu.Count > 0 Then
+                        Emp.Clear()
+                        Console.WriteLine(" All Succesfully deleted")
+                    Else
+                        Console.WriteLine("No student added yet.")
+                    End If
+                    Console.WriteLine()
+
+                Case 4
+                    Console.WriteLine("Exiting program...")
 
                 Case Else
-                    Console.WriteLine("Invalid choice")
+                    Console.WriteLine("Invalid choice. Please enter a valid option.")
+                    Console.WriteLine()
             End Select
-        End While
-    End Sub
-End Class
 
-Module Module1
-    Sub Main()
-        Dim program As New Program()
-        program.Run()
+        Loop While choice <> 3
     End Sub
 End Module
